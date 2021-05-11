1996ad03c2887577955e3790b9b7428b7ed611eb
Dan A <andersondanc@gmail.com>
2021-05-11 14:27:01 -0500

    Call to API now uses Axios to call Openweather API

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+
+var { WEATHER_KEY } = require('../key');
 
 /* GET  */
 router.get('/', function(req, res, next) {
-  res.send('Hello world!');
+  let lat = 'lat=' + '44.97';
+  let lon = '&lon=' + '-93.26';
+  axios.get('http://api.openweathermap.org/data/2.5/onecall?' + lat + lon + '&appid=' + WEATHER_KEY)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send(error);
+    });
 });
 
 module.exports = router;
